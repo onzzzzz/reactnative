b8818824eec16324709ab52980fc884a54fb533c
onzzzzz <ninaonz.shop@gmail.com>
2025-06-06 16:20:09 +0100

    home page

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center" >
-      <Text className="text-5xl text-primary font-bold">Hello</Text>
+      <Text className="text-5xl text-primary font-bold">Hello Leonardo
+      </Text>
     </View>
   );
 }
